b90b49cf26d3bbef2975e40b8152dfaa96094413
Dlamar <dlamardima@gmail.com>
2023-05-19 13:49:51 +0300

    project init

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const nextConfig = {
   basePath: process.env.BASE_PATH,
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    BASE_PATH:  process.env.BASE_PATH,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://pipedrive-v1.onrender.com/home/api/auth',
+    BASE_PATH:  process.env.BASE_PATH || '/home',
   }
 }
 
