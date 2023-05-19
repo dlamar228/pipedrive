f7c7d7d3fe38e54eb9a05fab0fb7aecb25be9ffa
Dlamar <dlamardima@gmail.com>
2023-05-19 16:25:39 +0300

    project init

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,5 +1,12 @@
+import SurfaceSDK, { Command, Modal } from '@pipedrive/app-extensions-sdk';
+import {useEffect} from "react";
+
+const sdk = SurfaceSDK();
 
 export default function Home() {
+    useEffect(async()=>{
+        sdk.initialize({ size: { height: 300,width:300 } });
+    },[]);
 
     return (
         <>
