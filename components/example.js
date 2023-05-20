d69ed3dd13c4653c9348281236643667fb1591f9
Dlamar <dlamardima@gmail.com>
2023-05-20 11:56:08 +0300

    project init

--- components/example.js
+++ components/example.js
@@ -2,27 +2,73 @@ import { useRouter } from 'next/router'
 import useSdk from "./useSdk";
 import {SdkContextProvider} from "./sdk";
 import {useSession} from "next-auth/react";
+import {ApiClient, DealsApi} from "pipedrive";
+import {useState} from "react";
 
 
 const Test = () =>{
     //const sdk = useSdk();
     const router = useRouter();
     const sessions = useSession();
+    const [response, setResponse] = useState();
+
+    const defaultClient = new ApiClient();
+    defaultClient.authentications.api_key.apiKey = router.query.token;
+
+    const client = new DealsApi(defaultClient);
+
+    const data = {
+        title: 'Deal of the century',
+        value: 10000,
+        currency: 'USD',
+        user_id: router.query.userId,
+        person_id: null,
+        org_id: 1,
+        stage_id: 1,
+        status: 'open',
+        expected_close_date: '2024-02-11',
+        probability: 60,
+        lost_reason: null,
+        visible_to: 1,
+        add_time: '2023-05-20',
+    }
+
 
     return(
      <div>
-         router:
-         {   Object.entries(router.query)
-             .map( ([key,value]) => `${key}: ${value}`)
-             .map(item => <div>{item}</div>)
-         }
+         <div>
+             router:
+             {
+                 Object.entries(router.query)
+                 .map( ([key,value]) => `${key}: ${value}`)
+                 .map(item => <div>{item}</div>)
+             }
+         </div>
 
-         sessions:
-         {   Object.entries(sessions.data || {})
-             .map( ([key,value]) => `${key}: ${value}`)
-             .map(item => <div>{item}</div>)
-         }
+        <div>
+            sessions:
+            {
+                Object.entries(sessions.data || {})
+                .map( ([key,value]) => `${key}: ${value}`)
+                .map(item => <div>{item}</div>)
+            }
+        </div>
+
+         <div>
+             response:
+             {
+                'NONE'
+             }
+         </div>
+
+         <button onClick={async ()=>{
+             const res = await client.addDeal(data);
+             console.log(res)
+             setResponse(res);
+
+         }}>Press me</button>
      </div>
+
     )
 }
 
